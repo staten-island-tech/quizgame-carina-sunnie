91536a7044bb2600fe8b4ac4d9d940d46dc8e355
CarinaAlessandro <71675000+CarinaAlessandro@users.noreply.github.com>
2021-03-24 23:35:55 -0400

    notes and template

---
+++ app.js
@@ -0,0 +1,20 @@
+// Let score
+// //array
+// Question: string
+// Answers: possible-array of objects or strings
+// Ex (string)
+// [“ciao”, “Nihao”, “Hola”]
+// CorrectAnswer:”hola”
+//  Ex (object)
+// {answeChoice “caio”, correct: false} caio and false is a property. If it’s a buzzfeed style quiz with different values then you’d also have value:integer
+// //
+// //Event to Start game ? Click start button or on page load (listening to an event)
+// Display quiz question or questions
+// For questions for each iterate to display Or
+// Display question one.
+// Listen for user input event.
+// Check if correct.
+// If correct display correct modal (popup) or check, update score
+// Else display false modal or x, Update score.
+// (Checking index of q) if question number is less than questions.length display next question
+// else show user score display score modal/ end modal or something
