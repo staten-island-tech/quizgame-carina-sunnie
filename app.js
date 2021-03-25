a8012b29a9fc2308b390549ac4b359b6bcbc85e6
CarinaAlessandro <71675000+CarinaAlessandro@users.noreply.github.com>
2021-03-25 10:47:16 -0400

    question format

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
 // Let score
+
 // //array
 // Question: string
 // Answers: possible-array of objects or strings
 // Ex (string)
 // [“ciao”, “Nihao”, “Hola”]
+
 // CorrectAnswer:”hola”
 //  Ex (object)
 // {answeChoice “caio”, correct: false} caio and false is a property. If it’s a buzzfeed style quiz with different values then you’d also have value:integer
+const theQuestions === [
+    { 
+    question: "blah blah blah ?",
+    answers: {
+        {answerChoice "first", correct: false},
+        {answerChoice "second", correct: false},
+        {answerChoice "third", correct: true},
+    }
+    }
+    { 
+        question: "blah blah blah ?",
+        answers: {
+            {answerChoice "first", correct: false},
+            {answerChoice "second", correct: false},
+            {answerChoice "third", correct: true},
+        }
+    }
+    { 
+    question: "blah blah blah ?",
+    answers: {
+        {answerChoice "first", correct: false},
+        {answerChoice "second", correct: false},
+        {answerChoice "third", correct: true},
+    }
+    }  //etc. or have choices then have correctAnswer at the end. put values inthere maybe
+];
+
 // //
 // //Event to Start game ? Click start button or on page load (listening to an event)
 // Display quiz question or questions
