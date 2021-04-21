17c57c6fdabc725e2be147ce349de4781cf04e8b
Sunnie J <71672149+SunnieJ@users.noreply.github.com>
2021-04-21 15:59:58 -0400

    hngghh stress grip

--- src/js/apps.js
+++ src/js/apps.js
@@ -6,9 +6,34 @@ const ansB = document.getElementById("b");
 const ansC = document.getElementById("c");
 const question = document.getElementById("question");
 const NextButton = document.getElementById("next-btn");
+const questionContainter = document.getElementById("q-container");
+const answerChoices = document.getElementById("answerchoices-box");
 
-function setNextQuestion() {}
+let shuffledQuestions, currentQuestionsIndex;
 
+//here put shtuff to start the quiz and the function shamarbledarngledingdong
+function startGame() {
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  //sorts questions randomly, Math.random gives # between 1 & 0, subtracting 0.5 will make it randomized
+  currentQuestionsIndex = 0;
+  setNextQuestion();
+}
+function showQuestions(question) {
+  question.innerText = question.question;
+  //thanks google my good frienderino/butigetit
+  //then output display questions herrree and like,,, according to whalen,,,
+  // Display question one.
+  // Listen for user input event.
+  // Check if correct.
+  // If correct display correct modal (popup) or check, update score
+  // Else display false modal or x, Update score.
+  // (Checking index of q) if question number is less than questions.length display next question
+  // else show user score display score modal/ end modal or something LATER OK*****
+}
+
+function setNextQuestion() {
+  showQuestions(shuffledQuestions[currentQuestionsIndex]);
+}
 // Let score
 let score = 0;
 
@@ -22,22 +47,6 @@ for (const i = 0; i < questions.length; i) {
   }
 } //ITS A BIG OL LOOP. BREAKS WHEN YOU THE NUMBER OF QUESTIONS ARE ANSWERED
 alert("YOUR SCORE IS: " + score + "/" + questions.length + ". Not bad.");
-
-let quizContainer = document.getElementbyId("quiz");
-let resultsContainer = document.getElementbyId("results");
-let submitButton = document.getElementbyId("submit");
-//here put shtuff to start the quiz and the function shamarbledarngledingdong
-function showQuestions(allQuestions, quizContainer) {
-  //thanks google my good frienderino/butigetit
-  //then output display questions herrree and like,,, according to whalen,,,
-  // Display question one.
-  // Listen for user input event.
-  // Check if correct.
-  // If correct display correct modal (popup) or check, update score
-  // Else display false modal or x, Update score.
-  // (Checking index of q) if question number is less than questions.length display next question
-  // else show user score display score modal/ end modal or something LATER OK*****
-}
 
 // //etc. or have choices then have correctAnswer at the end. put values inthere maybe
 
